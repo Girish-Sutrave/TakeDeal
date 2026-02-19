f551bc864bebbe19fe509e6356fbc57c0c662723
Girish <girishsutrave1234@gmail.com>
2026-02-19 21:31:00 +0530

    Add project files.

---
+++ Models/Category.cs
@@ -0,0 +1,16 @@
+﻿namespace TakeDeal.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        public required string Name { get; set; }
+
+        public int? ParentCategoryId { get; set; }
+
+        public Category? ParentCategory { get; set; }
+
+        public ICollection<Category>? SubCategories { get; set; }
+    }
+
+}
---
+++ Models/User.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace TakeDeal.Models
+{
+    [Index(nameof(Email), IsUnique = true)]
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+
+        public required string Name { get; set; }
+
+        public required string PasswordHash  { get; set; }
+
+        [EmailAddress]
+        public required string Email { get; set; }
+        [Phone]
+        [StringLength(10,MinimumLength =10)]
+        public required string Phone { get; set; }
+
+        public DateTime CreatedDate { get; set; }=DateTime.UtcNow;
+
+    }
+}
