b7a23b0909fcd63c8c1c887e9e204e4afa938cf4
Girish Sutrave <gsutrave123@gmail.com>
2026-02-20 22:19:39 +0530

    Update User.cs
    Removed the spaces

--- Models/User.cs
+++ Models/User.cs
@@ -8,19 +8,13 @@ namespace TakeDeal.Models
     {
         [Key]
         public int Id { get; set; }
-
-
         public required string Name { get; set; }
-
         public required string PasswordHash  { get; set; }
-
         [EmailAddress]
         public required string Email { get; set; }
         [Phone]
         [StringLength(10,MinimumLength =10)]
         public required string Phone { get; set; }
-
         public DateTime CreatedDate { get; set; }=DateTime.UtcNow;
-
     }
 }
